c384ad3eb149718e46d945c3197108ad4b298540
SandeepPS <pssandeep@gmail.com>
2022-12-28 00:16:07 +1100

    Added more functions in CourseJdbcRepository

--- learn-jpa-and-hibernate/src/main/java/com/pssandeep/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
+++ learn-jpa-and-hibernate/src/main/java/com/pssandeep/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import com.pssandeep.springboot.learnjpaandhibernate.course.Course;
+
 @Component
 public class CourseJdbcCommandLineRunner implements CommandLineRunner{
 
@@ -14,7 +16,13 @@ public class CourseJdbcCommandLineRunner implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		
 		System.out.println("Command Line Runner"); 
-		repository.insert();
+		repository.insert(new Course(1,"TEST1","AUTHOR1"));
+		repository.insert(new Course(2,"TEST2","AUTHOR2"));		
+		repository.insert(new Course(3,"TEST3","AUTHOR3"));
+		repository.deleteById(2);
+		System.out.println(repository.findByID(1));
+		System.out.println(repository.findByID(3));
+		System.out.println(repository.findAll());
 		
 	}
 	
